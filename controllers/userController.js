444263d8bb27e5545578774524a7c4001dc2eb76
AzharMoosa <azhar.moosa8@gmail.com>
2021-08-13 18:26:57 +0100

    Backend Authentication

---
+++ controllers/userController.js
@@ -0,0 +1,61 @@
+import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
+import User from "../models/User.js";
+
+// @desc        Login User
+// @route       POST /api/users/login
+// @access      Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
+// @desc        Register User
+// @route       POST /api/users
+// @access      Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Cannot Register User");
+  }
+});
+
+export { loginUser, registerUser };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import colors from "colors";
 import usersRoute from "./routes/users.js";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 connectDB();
 
@@ -10,6 +11,10 @@ const app = express();
 app.use(express.json({ extended: true }));
 
 app.use("/api/users", usersRoute);
+
+// Error Middleware
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server Started At Port ${PORT}`.cyan));
