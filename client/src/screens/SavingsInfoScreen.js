424789e92e2823799022a8e1a9c8fddef521405e
AzharMoosa <azhar.moosa8@gmail.com>
2021-08-15 12:16:43 +0100

    Savings Info Page Scaffold

--- client/src/screens/SavingsInfoScreen.js
+++ client/src/screens/SavingsInfoScreen.js
@@ -4,7 +4,32 @@ import Layout from "../components/layout/Layout";
 const SavingsInfoScreen = ({ match }) => {
   return (
     <Layout>
-      <h1>{match.params.id}</h1>
+      <div className="savings-info-container">
+        <div className="savings-info-title">
+          <h1 className="title">House</h1>
+          <h1 className="title">£ 50000</h1>
+        </div>
+
+        <div className="savings-info-nav">
+          <h4>View Info</h4>
+          <h4>Add Amount</h4>
+          <h4>Edit Info</h4>
+          <h4>Delete Info</h4>
+        </div>
+
+        <div className="savings-info-layout">
+          <div className="savings-info-progress"></div>
+          <div className="savings-info-payments">
+            <h3>Monthly Payments</h3>
+          </div>
+          <div className="savings-info-history">
+            <h3>History</h3>
+          </div>
+          <div className="savings-info-info">
+            <h3>Info</h3>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
