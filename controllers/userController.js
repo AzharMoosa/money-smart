649c58e669f82646a56e08bf3105b85fe93410b8
AzharMoosa <azhar.moosa8@gmail.com>
2021-08-15 14:18:20 +0100

    CRUD Functionality For User

--- controllers/userController.js
+++ controllers/userController.js
@@ -58,4 +58,87 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { loginUser, registerUser };
+// @desc        Get User Profile
+// @route       GET /api/users/profile
+// @access      Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+// @desc    Get User By ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+// @desc        Update User Profile
+// @route       PUT /api/users/profile
+// @access      Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.firstName = req.body.firstName || user.firstName;
+    user.lastName = req.body.lastName || user.lastName;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      firstName: updatedUser.firstName,
+      lastName: updatedUser.lastName,
+      email: updatedUser.email,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+// @desc    Delete User
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.remove();
+    res.json({ message: "User Removed" });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+export {
+  loginUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUserById,
+  deleteUser,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/userController.js";
+import {
+  loginUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUserById,
+  deleteUser,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").post(registerUser);
 router.route("/login").post(loginUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router.route("/:id").get(protect, getUserById).delete(protect, deleteUser);
 
 export default router;
