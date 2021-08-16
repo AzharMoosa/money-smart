a7acd1330c275e9f4c1e888511ba57877092383f
AzharMoosa <azhar.moosa8@gmail.com>
2021-08-16 11:15:49 +0100

    Doughnut Chart For Amount Saved

--- client/src/screens/SavingsInfoScreen.js
+++ client/src/screens/SavingsInfoScreen.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout/Layout";
 import { useDispatch, useSelector } from "react-redux";
 import { getSaving } from "../actions/savingActions";
 import Message from "../components/error/Message";
+import { Doughnut } from "react-chartjs-2";
 
 const SavingsInfoScreen = ({ match }) => {
   const dispatch = useDispatch();
@@ -13,6 +14,37 @@ const SavingsInfoScreen = ({ match }) => {
   useEffect(() => {
     dispatch(getSaving(match.params.id));
   }, [dispatch, match]);
+
+  const data = {
+    maintainAspectRatio: false,
+    responsive: false,
+    datasets: [
+      {
+        data: [60, 40],
+        backgroundColor: ["#4BC0C0", "#52616B"],
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  const plugins = [
+    {
+      beforeDraw: function (chart) {
+        var width = chart.width,
+          height = chart.height,
+          ctx = chart.ctx;
+        ctx.restore();
+        var fontSize = (height / 140).toFixed(2);
+        ctx.font = fontSize + "em Lato";
+        ctx.textBaseline = "top";
+        var text = "60%",
+          textX = Math.round((width - ctx.measureText(text).width) / 2),
+          textY = height / 2 - 6;
+        ctx.fillText(text, textX, textY);
+        ctx.save();
+      },
+    },
+  ];
 
   return (
     <Layout>
@@ -32,7 +64,31 @@ const SavingsInfoScreen = ({ match }) => {
               <h4>Delete Info</h4>
             </div>
             <div className="savings-info-layout">
-              <div className="savings-info-progress"></div>
+              <div className="savings-info-progress">
+                <Doughnut
+                  height={189}
+                  width={189}
+                  data={data}
+                  plugins={plugins}
+                  options={{
+                    tooltip: false,
+                    responsive: false,
+                    maintainAspectRatio: true,
+                  }}
+                />
+                <div className="savings-info-progress-saved">
+                  <h3>Current Saved</h3>
+                  <h4>£ {saving.amountSaved}</h4>
+                </div>
+                <div className="savings-info-progress-left">
+                  <h3>Amount Left</h3>
+                  <h4>£ {saving.amountRequired - saving.amountSaved}</h4>
+                </div>
+                <div className="savings-info-progress-total">
+                  <h3>Total Required</h3>
+                  <h4>£ {saving.amountRequired}</h4>
+                </div>
+              </div>
               <div className="savings-info-payments">
                 <h3>Monthly Payments</h3>
               </div>
