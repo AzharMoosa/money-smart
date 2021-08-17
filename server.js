d7399beb759053749c9b2c660c1a3e7f1290e93a
AzharMoosa <azhar.moosa8@gmail.com>
2021-08-17 14:05:42 +0100

    Upload Route

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ import usersRoute from "./routes/users.js";
 import savingsRoute from "./routes/savings.js";
 import transactionsRoute from "./routes/transactions.js";
 import receiptsRoute from "./routes/receipts.js";
+import uploadRoute from "./routes/uploads.js";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import path from "path";
 
 connectDB();
 
@@ -17,6 +19,10 @@ app.use("/api/users", usersRoute);
 app.use("/api/savings", savingsRoute);
 app.use("/api/transactions", transactionsRoute);
 app.use("/api/receipts", receiptsRoute);
+app.use("/api/upload", uploadRoute);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Error Middleware
 app.use(notFound);
