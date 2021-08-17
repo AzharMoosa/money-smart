cab4058ef933d4bc851c85bbddbcd56cbd8c61e8
AzharMoosa <azhar.moosa8@gmail.com>
2021-08-17 17:36:06 +0100

    Tesseract JS Extracts Name  and Amount From Receipt

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,8 @@
 import express from "express";
 import multer from "multer";
 import path from "path";
+import Tesseract from "tesseract.js";
+import fs from "fs";
 const router = express.Router();
 
 const storage = multer.diskStorage({
@@ -34,11 +36,37 @@ const upload = multer({
   },
 });
 
-router.post("/", upload.single("image"), (req, res) => {
+const extractData = (text) => {
+  const splits = text.split(/\r?\n/);
+  const name = splits[0].split(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g)[0];
+
+  let amount = 0;
+
+  for (let i = 0; i < splits.length; i++) {
+    if (splits[i].search(new RegExp("Total", "i")) == 0) {
+      amount = parseFloat(splits[i].replace(/[^\d.-]/g, ""));
+    }
+  }
+
+  return {
+    name,
+    amount,
+  };
+};
+
+router.post("/", upload.single("image"), async (req, res) => {
+  const file = await fs.readFileSync(`./${req.file.path}`);
+  let textData = "";
+
+  await Tesseract.recognize(file, "eng").then(({ data: { text } }) => {
+    textData = text;
+  });
+
+  const { name, amount } = extractData(textData);
   res.json({
-    name: "Test",
-    amount: "10.20",
-    type: "Food",
+    name,
+    amount,
+    type: "",
     image: `/${req.file.path}`,
   });
 });
