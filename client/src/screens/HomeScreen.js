f817a3513c8e21d3891e28c8252fe0dfc7783382
AzharMoosa <azhar.moosa8@gmail.com>
2021-08-17 11:40:12 +0100

    Dashboard Loads Transactions, Savings and User

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import Layout from "../components/layout/Layout";
 import HighestExpenses from "../components/transactions/HighestExpenses";
 import { useDispatch, useSelector } from "react-redux";
 import { getTransactions } from "../actions/transactionActions";
+import { getUserDetails } from "../actions/userActions";
+import { getSavings } from "../actions/savingActions";
 import Loading from "../components/loading/Loading";
 import Message from "../components/error/Message";
 
@@ -13,54 +15,83 @@ const HomeScreen = () => {
     (state) => state.userTransactionsList
   );
 
+  const userSavingsList = useSelector((state) => state.userSavingsList);
+
+  const userDetails = useSelector((state) => state.userDetails);
+
   const {
     loading: loadingTransactions,
     transactions,
     error: errorTransactions,
   } = userTransactionsList;
 
+  const {
+    loading: loadingSavings,
+    savings,
+    error: errorSavings,
+  } = userSavingsList;
+
+  const {
+    loading: loadingUserDetails,
+    user,
+    error: errorUserDetails,
+  } = userDetails;
+
   useEffect(() => {
     dispatch(getTransactions());
+    dispatch(getSavings());
+    dispatch(getUserDetails());
   }, [dispatch]);
 
   return (
     <Layout>
       {loadingTransactions && <Loading />}
       {errorTransactions && <Message error={errorTransactions} />}
-      {!loadingTransactions && transactions != null && (
-        <div className="home-container">
-          <div className="home-title">
-            <h1 className="title">Dashboard</h1>
-            <div className="user-icon">
-              <h3>John Doe</h3>
-              <div className="user-img"></div>
-            </div>
-          </div>
-
-          <div className="home-data-overview">
-            <div className="home-left">
-              <div className="monthly-spending-overview">
-                <h3>Monthly Spending</h3>
+      {loadingSavings && <Loading />}
+      {errorSavings && <Message error={errorSavings} />}
+      {loadingUserDetails && <Loading />}
+      {errorUserDetails && <Message error={errorUserDetails} />}
+      {!loadingTransactions &&
+        !loadingSavings &&
+        !loadingUserDetails &&
+        user != null &&
+        savings != null &&
+        transactions != null && (
+          <div className="home-container">
+            <div className="home-title">
+              <h1 className="title">Dashboard</h1>
+              <div className="user-icon">
+                <h3>
+                  {user.firstName} {user.lastName}
+                </h3>
+                <div className="user-img"></div>
               </div>
-              <div className="savings-overview">
-                <h3>Savings</h3>
-                <div className="savings-cards">
-                  <div className="savings-card">
-                    <h3>House</h3>
-                  </div>
-                  <div className="savings-card">
-                    <h3>Car</h3>
-                  </div>
-                  <div className="savings-card">
-                    <h3>Shoes</h3>
+            </div>
+
+            <div className="home-data-overview">
+              <div className="home-left">
+                <div className="monthly-spending-overview">
+                  <h3>Monthly Spending</h3>
+                </div>
+                <div className="savings-overview">
+                  <h3>Savings</h3>
+                  <div className="savings-cards">
+                    <div className="savings-card">
+                      <h3>House</h3>
+                    </div>
+                    <div className="savings-card">
+                      <h3>Car</h3>
+                    </div>
+                    <div className="savings-card">
+                      <h3>Shoes</h3>
+                    </div>
                   </div>
                 </div>
               </div>
+              <HighestExpenses transactions={transactions} />
             </div>
-            <HighestExpenses transactions={transactions} />
           </div>
-        </div>
-      )}
+        )}
     </Layout>
   );
 };
