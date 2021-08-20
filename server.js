5ae9fb14df7c548446c227516e63b66c86ac437c
AzharMoosa <azhar.moosa8@gmail.com>
2021-08-20 14:54:23 +0100

    Fixed Port

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Started At Port ${PORT}`.cyan));
